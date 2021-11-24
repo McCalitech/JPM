ae09815db8b2917585f17317475d0c59f63cc20a
McCalitech1 <uchemokwechime2021@gmail.com>
2021-11-24 15:13:45 -0800

    Updated DBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,3 @@
+The last Thursday of November, the United States celebrates Thanksgiving
+On this day, family and friends get together to celebrate with one another and share the good things that happened throughtout the year
+
